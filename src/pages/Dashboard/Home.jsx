219e72cc06c145c919488d41ba195560bffaec04
Trung Nam <trungnam.work@gmail.com>
2025-08-13 23:44:40 +0700

    feat: Filter By Poll Type Component

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,13 +1,32 @@
-import DashboardLayout from "../../components/layout/DashboardLayout"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useUserAuth from "../../hooks/useUserAuth";
+import DashboardLayout from "../../components/layout/DashboardLayout"
+import HeaderWithFilter from "../../components/layout/HeaderWithFilter";
 
 
 const Home = () => {
   useUserAuth();
 
+
+  const navigate = useNavigate();
+
+  const [allPolls, setAllPolls] = useState([]);
+  const [stats, setStats] = useState([]);
+  const [page, setPage] = useState(1);
+  const [filterType, setFilterType] = useState('');
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div>Home</div>
+      <div className="my-5 mx-auto">
+        <HeaderWithFilter
+          title="Polls"
+          filterType={filterType}
+          setFilterType={setFilterType}
+        />
+      </div>
     </DashboardLayout>
   )
 }
